ea169c6e21bb03c5e3ce60666cecfd2e3415e369
Steve Edouard <steven.edouard1@gmail.com>
2017-01-11 03:07:28 -0800

    Wip - fixes to integrate to VMPower dashboard

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
 /* jshint node: true */
 'use strict';
-
+var nodeSass = require('node-sass');
 module.exports = {
-  name: 'ember-notif-hub',
-
-  included: function (app) {
-    this._super.included.apply(this, arguments);
-    app.import('bower_components/material-design-icons/iconfont/material-icons.css');
-    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff', {
-        destDir: 'assets'
-    });
-    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2', {
-        destDir: 'assets'
-    });
-    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.svg', {
-        destDir: 'assets'
-    });
-    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ttf', {
-        destDir: 'assets'
-    });
-    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.eot', {
-        destDir: 'assets'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Regular.eot', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Regular.svg', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Regular.ttf', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Regular.woff', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Regular.woff2', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Light.woff2', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Light.svg', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Light.ttf', {
-        destDir: 'font/roboto'
-    });
-    app.import('bower_components/roboto-fontface/fonts/Roboto-Light.woff', {
-        destDir: 'font/roboto'
-    });
-  }
+    name: 'ember-notif-hub',
+    included: function (app) {
+        this._super.included.apply(this, arguments);
+        if (!app.sassOptions) {
+            app.sassOptions = {};
+            console.log('NO INCLUDE PATH');
+            app.sassOptions.includePaths = [];
+        }
+        app.sassOptions.nodeSass = nodeSass;
+        console.log('adding to INCLUDE!');
+        app.sassOptions.includePaths.push('app/styles');
+        this.ui.writeLine('Including Ember Notification Hub PNG Icons...');
+        // these come from the material-icons bower package
+        // but including the icon font woul dhadd up to 400-500kb to the app
+        app.import('vendor/icons/ic_done_white_24dp_2x.png', {
+            destDir: 'icons'
+        });
+        app.import('vendor/icons/ic_error_outline_white_24dp_2x.png', {
+            destDir: 'icons'
+        });
+        app.import('vendor/icons/ic_more_vert_white_24dp_2x.png', {
+            destDir: 'icons'
+        });
+    },
+    afterInstall: function () {
+        // Add addons to package.json and run defaultBlueprint
+        return this.addAddonsToProject({
+            // a packages array defines the addons to install
+            packages: [
+                // name is the addon name, and target (optional) is the version
+                {name: 'ember-localstorage-adapter'},
+                {name: 'ember-cli-sass'}
+            ]
+        });
+    }
 };
