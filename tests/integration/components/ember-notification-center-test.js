38405f9b33790ecf4fd63705b4863af53576a6f0
Steve Edouard <steven.edouard1@gmail.com>
2017-01-11 15:31:48 -0800

    Fix unit tests for image icons

--- tests/integration/components/ember-notification-center-test.js
+++ tests/integration/components/ember-notification-center-test.js
@@ -19,10 +19,9 @@ test('it renders default state', function (assert) {
     // Handle any actions with this.on('myAction', function(val) { ... });
     this.render(hbs `{{ember-notification-center bottom="-25px" openBottom="0px" left="20%" width="60%" pullDown=false}}`);
 
-    // icons
-    assert.equal(this.$().text().trim().indexOf('more_vert') > -1, true);
-    assert.equal(this.$().text().trim().indexOf('done') > -1, true);
-    assert.equal(this.$().text().trim().indexOf('error_outline') > -1, true);
+    // notification counts
+    assert.ok(this.$().text().trim().indexOf('0') > -1);
+    assert.ok(this.$().text().trim().indexOf('0') > -1);
     // expected default text
     assert.equal(this.$().text().trim().indexOf('Notifications') > -1, true);
 });
@@ -41,19 +40,21 @@ test('it renders pending and successful notification', function (assert) {
         }));
         assert.expect(11);
         this.render(hbs `{{ember-notification-center bottom="-25px" openBottom="0px" left="20%" width="60%" pullDown=false}}`);
-        assert.equal(this.$().text().trim().indexOf('1 Pending Tasks') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Integration Test Notification 1') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Some description 1') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Pending') > -1, true);
+        assert.ok(this.$().text().trim().indexOf('1 Pending Tasks') > -1);
+        assert.ok(this.$().text().trim().indexOf('Integration Test Notification 1') > -1);
+        assert.ok(this.$().text().trim().indexOf('Some description 1') > -1);
+        assert.ok(this.$().text().trim().indexOf('Pending') > -1);
         Ember.run.scheduleOnce('afterRender', () => {
-            assert.equal(this.$().text().trim().indexOf('Pending'), -1);
-            assert.equal(this.$().text().trim().indexOf('Success') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('done1') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('error_outline0') > -1, true);
+            assert.ok(this.$().text().trim().indexOf('Pending') === -1);
+            assert.ok(this.$().text().trim().indexOf('Success') > -1);
+            // # of errors should be 1
+            assert.ok(this.$().text().trim().indexOf('1') > -1);
+            // # of successe should be 0
+            assert.ok(this.$().text().trim().indexOf('0') > -1);
             // verify a notification is in the store
             var store = this.container.lookup('service:store');
             var notifs = store.peekAll('emberNotificationLocalNotification');
-            // there should only be 1 notification
+            // there should only be 1 notification model
             notifs.forEach(notif => {
                 assert.equal(notif.get('status'), 'Success');
                 assert.equal(notif.get('description'), 'Some description 1');
@@ -85,21 +86,21 @@ test('it renders pending and failure notification', function (assert) {
             ]);
         }));
         this.render(hbs `{{ember-notification-center bottom="-25px" openBottom="0px" left="20%" width="60%" pullDown=false}}`);
-        assert.equal(this.$().text().trim().indexOf('1 Pending Tasks') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Integration Test Notification 1') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Some description 1') > -1, true);
-        assert.equal(this.$().text().trim().indexOf('Pending') > -1, true);
+        assert.ok(this.$().text().trim().indexOf('1 Pending Tasks') > -1);
+        assert.ok(this.$().text().trim().indexOf('Integration Test Notification 1') > -1);
+        assert.ok(this.$().text().trim().indexOf('Some description 1') > -1);
+        assert.ok(this.$().text().trim().indexOf('Pending') > -1);
         Ember.run.scheduleOnce('afterRender', () => {
             // status text
             assert.equal(this.$().text().trim().indexOf('Pending'), -1);
             // title bar text
-            assert.equal(this.$().text().trim().indexOf('Failed: Some description 2') > -1, true);
+            assert.ok(this.$().text().trim().indexOf('Failed: Some description 2') > -1);
             // failed icon counter
-            assert.equal(this.$().text().trim().indexOf('error_outline1') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('404 Not Found') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('We cannot find that scrumptious bagel you are looking for') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('403 Unauthorized') > -1, true);
-            assert.equal(this.$().text().trim().indexOf('You do not have access to this nifty resource') > -1, true);
+            assert.ok(this.$().text().trim().indexOf('1') > -1);
+            assert.ok(this.$().text().trim().indexOf('404 Not Found') > -1);
+            assert.ok(this.$().text().trim().indexOf('We cannot find that scrumptious bagel you are looking for') > -1);
+            assert.ok(this.$().text().trim().indexOf('403 Unauthorized') > -1);
+            assert.ok(this.$().text().trim().indexOf('You do not have access to this nifty resource') > -1);
         });
     });
 });
