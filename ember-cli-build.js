661059a498b84e1629b00f1bb7a134a813d8ad39
Steve Edouard <steven.edouard1@gmail.com>
2017-01-11 14:54:41 -0800

    Mobile-friendly media queries

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,19 +3,35 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 var nodeSass = require('node-sass')
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    nodeSass: nodeSass,
-    includePaths: [
-      'app/styles'
-    ]
-  });
+    var app = new EmberAddon(defaults, {
+        nodeSass: nodeSass,
+        includePaths: [
+          'app/styles'
+        ]
+    });
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-  app.import('bower_components/materialize/dist/css/materialize.css')
-  return app.toTree();
+    /*
+      This build file specifies the options for the dummy test app of this
+      addon, located in `/tests/dummy`
+      This build file does *not* influence how the addon or the app using it
+      behave. You most likely want to be modifying `./index.js` or app's build file
+    */
+    app.import('bower_components/materialize/dist/css/materialize.css');
+    app.import('bower_components/material-design-icons/iconfont/material-icons.css');
+    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff', {
+        destDir: 'assets'
+    });
+    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2', {
+        destDir: 'assets'
+    });
+    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.svg', {
+        destDir: 'assets'
+    });
+    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ttf', {
+        destDir: 'assets'
+    });
+    app.import('bower_components/material-design-icons/iconfont/MaterialIcons-Regular.eot', {
+        destDir: 'assets'
+    });
+    return app.toTree();
 };
